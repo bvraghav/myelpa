922b548ba11d0e1353e7ca699dcc98c0b3b44c0d
B.V. Raghav <bvraghav@gmail.com>
2018-06-30 01:57:24 +0530

    Adding myelpa setup

---
+++ myelpa-setup.el
@@ -0,0 +1,18 @@
+;; Adding the submodules to loadpath
+
+(add-to-list 'load-path
+	     (let ((default-directory
+		     (file-name-directory
+		      (or load-file-name
+			  buffer-file-name))))
+	       (mapcar
+		(lambda (x)
+		  (concat (file-name-as-directory default-directory)
+			  x))
+		(seq-filter
+		 (lambda (x)
+		   (and (file-directory-p x)
+			(not (string-prefix-p "." x))))
+		 (directory-files default-directory)))))
+
+(provide myelpa-setup)
