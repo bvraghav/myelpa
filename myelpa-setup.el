233addcb444b04685a57513728e8583d673bb2e3
B.V. Raghav <bvraghav@gmail.com>
2018-07-26 11:09:30 +0530

    Append list instead of adding. (Error: add-to-list expected string; list given)

--- myelpa-setup.el
+++ myelpa-setup.el
@@ -1,18 +1,19 @@
 ;; Adding the submodules to loadpath
 
-(add-to-list 'load-path
-	     (let ((default-directory
-		     (file-name-directory
-		      (or load-file-name
-			  buffer-file-name))))
-	       (mapcar
-		(lambda (x)
-		  (concat (file-name-as-directory default-directory)
-			  x))
-		(seq-filter
+(setq load-path
+      (append load-path
+	      (let ((default-directory
+		      (file-name-directory
+		       (or load-file-name
+			   buffer-file-name))))
+		(mapcar
 		 (lambda (x)
-		   (and (file-directory-p x)
-			(not (string-prefix-p "." x))))
-		 (directory-files default-directory)))))
+		   (concat (file-name-as-directory default-directory)
+			   x))
+		 (seq-filter
+		  (lambda (x)
+		    (and (file-directory-p x)
+			 (not (string-prefix-p "." x))))
+		  (directory-files default-directory))))))
 
-(provide myelpa-setup)
+(provide 'myelpa-setup)
