8f8e35a00f0c15914c801e8eb52a9320fa60f485
B V Raghav <bvraghav@gmail.com>
2018-11-22 16:14:57 +0530

    Use nconc instead of append. Append creates copy.

--- myelpa-setup.el
+++ myelpa-setup.el
@@ -1,19 +1,19 @@
 ;; Adding the submodules to loadpath
 
-(setq load-path
-      (append load-path
-	      (let ((default-directory
-		      (file-name-directory
-		       (or load-file-name
-			   buffer-file-name))))
-		(mapcar
-		 (lambda (x)
-		   (concat (file-name-as-directory default-directory)
-			   x))
-		 (seq-filter
-		  (lambda (x)
-		    (and (file-directory-p x)
-			 (not (string-prefix-p "." x))))
-		  (directory-files default-directory))))))
+(setq melpa-path
+      (let ((default-directory
+	      (file-name-directory
+	       (or load-file-name
+		   buffer-file-name))))
+	(mapcar
+	 (lambda (x)
+	   (concat (file-name-as-directory default-directory)
+		   x))
+	 (seq-filter
+	  (lambda (x)
+	    (and (file-directory-p x)
+		 (not (string-prefix-p "." x))))
+	  (directory-files default-directory)))))
+(nconc load-path melpa-path)
 
 (provide 'myelpa-setup)
